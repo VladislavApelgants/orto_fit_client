9027f424035f75c980fdab3d9757784dcb8b3dc7
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2025-03-05 17:14:39 +0200

    add base mobile menu

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,33 +1,33 @@
-import {LangSelector} from "@/components/shared/LangSelector/LangSelector";
-import {getTranslate} from "@/tolgee/server";
+import { Cart } from "@/components/layout/Header/Cart/Cart";
+import { LangSelector } from "@/components/shared/LangSelector/LangSelector";
+import { getTranslate } from "@/tolgee/server";
 import Link from "next/link";
-import {JSX} from "react";
+import { JSX } from "react";
 import s from "./header.module.scss";
-import {Nav} from "./Nav/Nav";
-import {MenuButton} from "@/components/shared/MenuButton/MenuButton";
-import {Cart} from "@/components/layout/Header/Cart/Cart";
+import { MobileMenu } from "./MobMenu/MobMenu";
+import { Nav } from "./Nav/Nav";
 
 export const Header = async (): Promise<JSX.Element> => {
-    const t = await getTranslate();
-    console.log("😎 ~ Header ~  t:", t("header.products"));
+  const t = await getTranslate();
+  console.log("😎 ~ Header ~  t:", t("header.products"));
 
-    return (
-        <header className={s.header}>
-            <div className={`container`}>
-                <div className={s.header_wrapper}>
-                    <div className={s.header_nav_block}>
-                        <Link href="/" className={s.header_logo}>
-                            MaryOrtoFit
-                        </Link>
-                        <Nav navClass={s.nav_block}/>
-                        <MenuButton/>
-                    </div>
-                    <div className={s.header_service_block}>
-                        <Cart/>
-                        <LangSelector/>
-                    </div>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className={s.header}>
+      <div className={`container`}>
+        <div className={s.header_wrapper}>
+          <div className={s.header_nav_block}>
+            <Link href="/" className={s.header_logo}>
+              MaryOrtoFit
+            </Link>
+            <Nav navClass={s.nav_block} />
+            <MobileMenu />
+          </div>
+          <div className={s.header_service_block}>
+            <Cart />
+            <LangSelector />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
--- src/components/layout/Header/MobMenu/MobMenu.tsx
+++ src/components/layout/Header/MobMenu/MobMenu.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+"use client";
 
-export const MobMenu = (): React.JSX.Element => {
-  return <div></div>;
+import { useState } from "react";
+import s from "./mobMenu.module.scss";
+
+export const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleOpenMobileMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button
+        onClick={toggleOpenMobileMenu}
+        className={`${s.burger_btn} ${isOpen ? s.open : ""}`}
+      >
+        <span></span>
+        {/* <span></span> */}
+        <span></span>
+      </button>
+      <div
+        className={`${s.mob_menu_container} ${
+          isOpen ? s.openModal : s.closedModal
+        }`}
+      >
+        <nav className=""></nav>
+      </div>
+    </>
+  );
 };
