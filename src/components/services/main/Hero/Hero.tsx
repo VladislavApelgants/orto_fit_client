b66fd226c835ad5b19820aad2138d6f1c6d4307c
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2025-03-21 17:35:24 +0200

    fix hero image

--- src/components/services/main/Hero/Hero.tsx
+++ src/components/services/main/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { JSX } from "react";
 import s from "./hero.module.scss";
@@ -5,6 +6,16 @@ import s from "./hero.module.scss";
 export const Hero = (): JSX.Element => {
   return (
     <section className={s.hero_section}>
+      <div className={s.image_wrapper}>
+        <Image
+          src="/hero.webp"
+          alt="Hero background"
+          fill
+          priority
+          sizes="100vw"
+          className={s.hero_image}
+        />
+      </div>
       <div className="container">
         <div className={s.info_block}>
           <p className={s.subtitle}>Say Hello to ToyStore!</p>
