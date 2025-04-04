92b62cb54b5bcb0b6834ee3a3736e80a65805837
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2025-04-04 18:26:37 +0300

    add cart page

--- src/components/services/components/Cart/Cart.tsx
+++ src/components/services/components/Cart/Cart.tsx
@@ -1,10 +1,15 @@
 import { Icon } from "@/components/ui/Icon/Icon";
+import Link from "next/link";
 import { JSX } from "react";
 import s from "./cart.module.scss";
 
 export const Cart = (): JSX.Element => {
   return (
-    <button className={s.cart_btn} aria-label="Кнопка кошику товарів">
+    <Link
+      href="/cart"
+      className={s.cart_btn}
+      aria-label="Кнопка кошику товарів"
+    >
       <Icon
         type="shopBag"
         height={24}
@@ -14,6 +19,6 @@ export const Cart = (): JSX.Element => {
         classnames={s.icon_cart}
       />
       <span className={s.count}>12</span>
-    </button>
+    </Link>
   );
 };
